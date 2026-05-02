377d7d5fc158e0e107568ef2c92ac055ce3700df
Adit2006-bit <vsri5658@gmail.com>
2026-05-02 15:21:40 +0530

    fix create board api route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,22 @@ import axios from "axios";
 import Whiteboard from "./Whiteboard";
 import "./App.css";
 
+const API_URL = "https://collaborative-whiteboard-backend-3aio.onrender.com";
+
 function App() {
   const [boardId, setBoardId] = useState("");
   const [joinedBoard, setJoinedBoard] = useState(false);
   const [permission, setPermission] = useState("edit");
 
   const createBoard = async () => {
-    const res = await axios.post("https://collaborative-whiteboard-backend-3aio.onrender.com");
-    setBoardId(res.data.boardId);
-    setJoinedBoard(true);
+    try {
+      const res = await axios.post(`${API_URL}/api/boards/create`);
+      setBoardId(res.data.boardId);
+      setJoinedBoard(true);
+    } catch (error) {
+      console.error("Error creating board:", error);
+      alert("Failed to create board");
+    }
   };
 
   const joinBoard = () => {
